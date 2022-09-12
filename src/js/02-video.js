2ade6cc949b59eeb78679133a05957e884112b39
VladosSid <sidorsv121097@gmail.com>
2022-09-12 19:10:46 +0300

    fix

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,15 +2,22 @@ import Player from '@vimeo/player';
 
 import * as _ from 'lodash';
 
+checkLocal();
+function checkLocal() {
+  return localStorage.getItem('videoplayer-current-time') === null
+    ? 0
+    : localStorage.getItem('videoplayer-current-time');
+}
+
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+player.setCurrentTime(checkLocal());
 
 const saveSett = player.on(
   'timeupdate',
   _.throttle(el => {
     console.log(el.seconds);
     localStorage.setItem('videoplayer-current-time', el.seconds);
-  }, 250)
+  }, 1000)
 );
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,4 @@
 import * as _ from 'lodash';
-// import { fromPairs } from 'lodash';
 
 const form = document.querySelector('.feedback-form');
 console.log(form);
@@ -10,9 +9,13 @@ const localData = {};
 function updateForm() {
   try {
     const jsData = JSON.parse(localStorage.getItem('feedback-form-state'));
-    console.log(jsData);
-    form.elements.email.value = jsData.email;
-    form.elements.message.value = jsData.message;
+
+    jsData.email === undefined
+      ? ''
+      : (form.elements.email.value = jsData.email);
+    jsData.message === undefined
+      ? ''
+      : (form.elements.message.value = jsData.message);
   } catch (error) {
     console.log(error.name);
     console.log(error.message);
@@ -31,8 +34,18 @@ function updateForm() {
 form.addEventListener(
   'input',
   _.throttle(el => {
+    const jsData = JSON.parse(localStorage.getItem('feedback-form-state'));
+
+    if (jsData !== null) {
+      jsData.email === undefined
+        ? (localData.email = '')
+        : (localData.email = jsData.email);
+      jsData.message === undefined
+        ? (localData.message = '')
+        : (localData.message = jsData.message);
+    }
+
     localData[el.target.name] = el.target.value;
-    console.log(localData);
 
     localStorage.setItem('feedback-form-state', JSON.stringify(localData));
   }, 500)
